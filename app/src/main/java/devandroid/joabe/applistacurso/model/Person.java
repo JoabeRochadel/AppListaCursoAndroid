44edf6fe58075530a6dda4f302943b66f1064606
Joabe Rochadel <joaberochadel@gmail.com>
2023-03-12 12:06:07 -0300

    criacao propriedades Person

--- app/src/main/java/devandroid/joabe/applistacurso/model/Person.java
+++ app/src/main/java/devandroid/joabe/applistacurso/model/Person.java
@@ -1,5 +1,42 @@
 package devandroid.joabe.applistacurso.model;
 
 public class Person {
+    private String name;
+    private String secondName;
+    private String nameCourse;
+    private String numberContact;
 
+    public Person(){}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getNameCourse() {
+        return nameCourse;
+    }
+
+    public void setNameCourse(String nameCourse) {
+        this.nameCourse = nameCourse;
+    }
+
+    public String getNumberContact() {
+        return numberContact;
+    }
+
+    public void setNumberContact(String numberContact) {
+        this.numberContact = numberContact;
+    }
 }
