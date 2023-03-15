9ed821accc3c2f98991b8eaec4fb9b6d3ba737f4
Joabe Rochadel <joaberochadel@gmail.com>
2023-03-15 08:28:28 -0300

    criacao de layout com spinner curso solicitado

--- app/src/main/java/devandroid/joabe/applistacurso/view/MainActivity.java
+++ app/src/main/java/devandroid/joabe/applistacurso/view/MainActivity.java
@@ -36,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_spinner);
         personController = new PersonController(MainActivity.this);
         requestedCourseController = new RequestedCourseController();
         person = new Person();
